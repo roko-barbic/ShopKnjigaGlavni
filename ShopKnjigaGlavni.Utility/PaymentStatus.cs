80c17afabd4b2defa90cff3c91787fee4c5cbd08
roko-barbic <rokobarosk12345@gmail.com>
2024-05-08 17:19:43 +0200

    add missing utilitys

---
+++ ShopKnjigaGlavni.Utility/PaymentStatus.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopKnjigaGlavni.Utility;
+
+public class PaymentStatus
+{
+    public const string Pending = "Pending";
+    public const string Approved = "Approved";
+    public const string Delayed = "Delayed";
+    public const string Rejected = "Rejected";
+}
